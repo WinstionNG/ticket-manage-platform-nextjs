3711dc9098b90929d591a803754d7cb1ad8967d9
Xuan Wu <xuanwu@Xuans-MacBook-Pro.local>
2024-09-07 23:26:01 +0800

    add spinner

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { z } from 'zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { zodResolver } from '@hookform/resolvers/zod'
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -19,6 +20,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema)
   })
   const [error, setError] = useState<string>()
+  const [submitLoading, setSubmitLoading] = useState(false)
   
   return (
     <div className='max-w-xl'>
@@ -29,10 +31,13 @@ const NewIssuePage = () => {
         className='space-y-3'
         onSubmit={handleSubmit(async(data) => {
             try {
+                setSubmitLoading(true)
                 await axios.post('/api/issues', data)
                 router.push('/issues')
             }catch(err) {
                 setError('An unexpect error occur!')
+            }finally {
+                setSubmitLoading(false)
             }
         })}
     >
@@ -46,7 +51,7 @@ const NewIssuePage = () => {
         render={({field}) => <SimpleMDE placeholder='description' {...field}/>}
       />
       <ErrorMessage>{errors.description?.message}</ErrorMessage>
-      <Button>Submit New Issue</Button>
+      <Button disabled={submitLoading}>Submit New Issue {submitLoading && <Spinner/>}</Button>
     </form>
     </div>
   )
